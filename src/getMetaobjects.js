9fb7c5c564bfe9ece4340f21b7b4759e3801b8c2
AlexBeskrovniy <aleksandr.beskrovniy.90@gmail.com>
2023-09-19 07:33:32 +0500

    Read metaobjects

--- src/getMetaobjects.js
+++ src/getMetaobjects.js
@@ -1,5 +1,11 @@
 import 'dotenv/config';
+import * as fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { shopify } from "./index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const session = shopify.session.customAppSession(process.env.SHOP_NAME);
 
@@ -7,15 +13,31 @@ const queryString = `{
     metaobjects (type: "theme_color_swatches", first: 100) {
       edges {
         node {
-          handle
+            handle
+            type
+            color: field(key: "color") { value }
+            label: field(key: "label") { value }
+            image: field(key: "image") { value }
         }
       }
     }
   }`
 
   const client = new shopify.clients.Graphql({ session });
-  const products = await client.query({
+  const response = await client.query({
     data: queryString,
   });
 
-  console.log(products.body.data.metaobjects.edges);
+const defs = response.body.data.metaobjects.edges.map(metaObj => {
+    return {
+        handle: metaObj.node.handle,
+        type: metaObj.node.type,
+        fields: {
+            color: metaObj.node.color.value,
+            label: metaObj.node.label.value
+        }
+    }
+});
+
+fs.writeFileSync(path.join(__dirname, 'metaobjects.json'), JSON.stringify(defs, null, '\t'));
+console.log(defs);
